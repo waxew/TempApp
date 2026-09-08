1878a5903f0bb70ac66d00429e1f27102ca4704f
waxew <4622.ar@gmail.com>
2026-09-08 22:40:16 +0330

    Add TempApp Kotlin template

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace="com.tempapp.template"; compileSdk=35
+ defaultConfig { applicationId="com.tempapp.template"; minSdk=24; targetSdk=35; versionCode=1; versionName="1.0.0" }
+}
